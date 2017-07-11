485c1bb2cc153c04f8946c718b9bedd6fe18e6c3
Marc Culler <culler@users.noreply.github.com>
2017-07-11 14:37:29 -0500

    Set up a simple form in the testapp.

--- test/testapp/views.py
+++ test/testapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from djatex import render_latex
-
-# Create your views here.
+from .forms import ArticleForm
 
 def index(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        
+        if form.is_valid():
+            author = form.cleaned_data['author']
+            title = form.cleaned_data['title']
+            context={
+                'form': form,
+                'author': author,
+                'title': title
+            }
+            return render_latex('testfile.pdf', 'testapp/test.tex', context=context)
+    else:
+        form = ArticleForm()
+        context = {'form': form}
+    return render(request, 'testapp/index.html', context) 
+
+def download(request):
     context = {'author': b'Joe Blow', 'title': b'An old result'}
-    return render_latex('testfile.pdf', 'testapp/test.tex', context=context)
